ced674275dae396e280adce72a6da6b824296ec9
RafaelSoaresA <114888968+RafaelSoaresA@users.noreply.github.com>
2025-04-01 10:29:23 -0300

    Delete ms.teachers.management/ms.teachers.management directory

--- ms.teachers.management/ms.teachers.management/src/main/java/com/example/ms/teachers/management/core/port/out/SendEventProducerPortOut.java
+++
@@ -1,8 +0,0 @@
-package com.example.ms.teachers.management.core.port.out;
-
-import com.example.ms.teachers.management.core.port.out.dto.TeacherListDtoOutput;
-
-public interface SendEventProducerPortOut {
-
-    void sendTeacherInfoEvent(TeacherListDtoOutput teacherListDtoOutput);
-}
